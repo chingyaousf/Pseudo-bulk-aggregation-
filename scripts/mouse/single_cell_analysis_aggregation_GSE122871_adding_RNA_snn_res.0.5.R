954f3ff4dc4b998ae3cb98ff1e19fdcb03a4d048
chingyaousf <96981462+chingyaousf@users.noreply.github.com>
2024-02-13 17:05:59 -0500

    Add files via upload

---
+++ scripts/mouse/single_cell_analysis_aggregation_GSE122871_adding_RNA_snn_res.0.5.R
@@ -0,0 +1,43 @@
+#clean up your environment
+rm(list=ls())
+
+# load library
+library(Seurat)
+library(SeuratData)
+library(SeuratDisk)
+
+#Load Seurat Object:
+h5_friendly.h5seurat <- LoadH5Seurat("C:/Users/Administrator/Desktop/single_cell_analysis_aggregation/mouse_batch2/GSE122871_h5friendly.h5seurat") 
+
+# Create the directory if it doesn't exist
+output_folder <- "C:/Users/Administrator/Desktop/single_cell_analysis_aggregation/mouse_batch2/GSE122871_h5friendly.h5seurat_pseudo_bulk_adding_RNA_snn_res.0.5"
+if (!dir.exists(output_folder)) {
+  dir.create(output_folder, recursive = TRUE)
+}
+
+# Save the metadata to a text file in the specified path with column names
+write.table(h5_friendly.h5seurat@meta.data, file.path(output_folder, "GSE122871_h5friendly.h5seurat_meta.txt"), sep="\t", row.names = TRUE, col.names = TRUE)
+
+# Display Head of Metadata:
+head(h5_friendly.h5seurat@meta.data) 
+
+# Aggregate Expression Data
+agg = AggregateExpression(h5_friendly.h5seurat, return.seurat = T, group.by = c('orig.ident', 'seurat_clusters_gabby_annotation', 'RNA_snn_res.0.5'), normalization.method = "LogNormalize",scale.factor = 10000)
+
+head(agg)
+
+# generating pseudo bulk level meta file
+cnames = colnames(agg@assays$RNA$data)
+meta1 = do.call(rbind, strsplit(cnames, "_")) 
+meta_text = cbind(cnames, meta1)
+
+# Specify File Paths:
+file <- file.path(output_folder, "GSE122871_expression_matrix_adding_RNA_snn_res.0.5.txt")
+meta_file <- file.path(output_folder, "GSE122871_meta_information_adding_RNA_snn_res.0.5.txt")
+
+# Write Expression Matrix to File:
+write.table(2^ agg@assays$RNA$data, file = file, sep="\t", row.names = TRUE,col.names=NA)
+
+# Write Meta Information to File:
+write.table(meta_text, file = meta_file, sep="\t", row.names=F) 
+
